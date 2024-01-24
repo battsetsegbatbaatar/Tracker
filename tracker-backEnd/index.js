789b5f2f329865e16434b00186d2cabd1917edd6
battsetsegbatbaatar <147494554+battsetsegbatbaatar@users.noreply.github.com>
2024-01-24 20:25:33 +0800

    tracked

--- tracker-backEnd/index.js
+++ tracker-backEnd/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { sql } = require("./db");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const PORT = 8080;
@@ -17,14 +18,58 @@ app.get("/neon", async (req, res) => {
   res.send(data);
 });
 
-app.post("/users/", async (req, res) => {
-  const body = res.body;
-  const { carName, modal, year } = body;
-  const data = await sql`INSERT INTO cars {brand, modal, year }
-    VALUES (${carName}, ${modal}, ${year});
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    console.log(req.body);
+    // validator hiih
+    if (!name || !password || !email) {
+      return res.status(400).send("Fields are required");
+    }
+    // bycrigt
+    const encryptedPassword = await bcrypt.hash(password, 18);
+
+    const data =
+      await sql`INSERT INTO users (name,email,password,avatarImg,createAt,updateAt)
+    VALUES (${name}, ${email}, ${encryptedPassword},"",${date},${date});
     `;
-  res.send(data);
+    res.send(data);
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send("Email and password are required");
+  }
+
+  try {
+    const userData = await sql`SELECT * FROM users WHERE email = ${email}`;
+
+    if (!userData || !userData.length) {
+      return res.status(401).send("Invalid email or password");
+    }
+
+    const hashedPassword = userData[0].password;
+
+    const passwordMatch = await bcrypt.compare(password, hashedPassword);
+
+    if (!passwordMatch) {
+      return res.status(401).send("Invalid email or password");
+    }
+
+    // Handle successful login
+    res.send("Login successful");
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 app.listen(PORT, () => {
   console.log("Application running at http://localhost:" + PORT);
 });
